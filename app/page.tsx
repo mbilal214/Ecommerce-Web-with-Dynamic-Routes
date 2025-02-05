eeb2e3585d420bd52ffac02807165ae173eb020b
mbilal214 <159235302+mbilal214@users.noreply.github.com>
2025-02-05 23:19:28 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
       <div className="p-6 text-center bg-gradient-to-r from-blue-500 to-purple-600 text-white">
         <h1 className="text-5xl font-bold mb-4">Welcome to My Store</h1>
         <p className="text-xl mb-4">Your best destination for quality products.</p>
-        <h2 className="text-lg italic">"Quality over quantity!"</h2>
+        <h2 className="text-lg italic">Quality over quantity!</h2>
       </div>
       <div className="mt-72">
       <Footer />
